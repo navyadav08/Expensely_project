ec24e490e1577a1600dff651ba3c6fa6261ef844
govindheda070504 <govindmaheshwari397@gmail.com>
2025-06-28 15:03:33 +0530

    dependency

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import FireAuth from './src/pages/fireauth';
+// import FireAuth from './src/pages/fireauth';
+import OCR from './src/ocr/ocr'; 
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -8,7 +9,7 @@ const App = () => {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <FireAuth />
+      <OCR />
     </View>
   );
 };
